028716fdfd2c8500bb8d3cef9bb13c81d69bf7ed
Gavin Killough <gavink@synopsys.com>
2019-01-21 13:47:31 -0500

    Add initial generated models

---
+++ src/main/java/com/synopsys/integration/polaris/common/api/Aggregation.java
@@ -0,0 +1,122 @@
+/*
+ * Common Object Service - Navigation
+ *
+ * Copyright (C) 2019 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.synopsys.integration.polaris.common.api;
+
+import com.synopsys.integration.util.Stringable;
+
+import java.util.Objects;
+import java.util.Arrays;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.synopsys.integration.polaris.common.api.AggregationGroup;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+// this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
+
+public class Aggregation extends Stringable {
+    @SerializedName("group")
+     private List<String> group = null;
+
+    @SerializedName("type")
+    private String type;
+
+    @SerializedName("when")
+    private String when;
+
+    @SerializedName("results")
+     private List<AggregationGroup> results = null;
+
+    public Aggregation addGroupItem(String groupItem) {
+        if (this.group == null) {
+            this.group = new ArrayList<>();
+        }
+        this.group.add(groupItem);
+        return this;
+    }
+
+    /**
+     * Get group
+     * @return group
+     */
+    public List<String> getGroup() {
+        return group;
+    }
+
+    public void setGroup(List<String> group) {
+        this.group = group;
+    }
+
+    /**
+     * Get type
+     * @return type
+     */
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Get when
+     * @return when
+     */
+    public String getWhen() {
+        return when;
+    }
+
+    public void setWhen(String when) {
+        this.when = when;
+    }
+
+    public Aggregation addResultsItem(AggregationGroup resultsItem) {
+        if (this.results == null) {
+            this.results = new ArrayList<>();
+        }
+        this.results.add(resultsItem);
+        return this;
+    }
+
+    /**
+     * Get results
+     * @return results
+     */
+    public List<AggregationGroup> getResults() {
+        return results;
+    }
+
+    public void setResults(List<AggregationGroup> results) {
+        this.results = results;
+    }
+
+}
---
+++ src/main/java/com/synopsys/integration/polaris/common/api/RunV0SubmittingOrganizationToOneRelationship.java
@@ -0,0 +1,75 @@
+/*
+ * Common Object Service - Navigation
+ *
+ * Copyright (C) 2019 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.synopsys.integration.polaris.common.api;
+
+import com.synopsys.integration.util.Stringable;
+
+import java.util.Objects;
+import java.util.Arrays;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.synopsys.integration.polaris.common.api.JsonApiRelationshipLinks;
+import com.synopsys.integration.polaris.common.api.SubmittingOrganizationV0RelationshipTarget;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.IOException;
+
+// this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
+
+public class RunV0SubmittingOrganizationToOneRelationship extends Stringable {
+    @SerializedName("links")
+    private JsonApiRelationshipLinks links = null;
+
+    @SerializedName("data")
+    private SubmittingOrganizationV0RelationshipTarget data = null;
+
+    /**
+     * Get links
+     * @return links
+     */
+    public JsonApiRelationshipLinks getLinks() {
+        return links;
+    }
+
+    public void setLinks(JsonApiRelationshipLinks links) {
+        this.links = links;
+    }
+
+    /**
+     * Get data
+     * @return data
+     */
+    public SubmittingOrganizationV0RelationshipTarget getData() {
+        return data;
+    }
+
+    public void setData(SubmittingOrganizationV0RelationshipTarget data) {
+        this.data = data;
+    }
+
+}
